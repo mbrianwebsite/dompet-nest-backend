6bd647cb1f0200b2076855f92a4744eee0615fe4
Muhammad Brian Ivan Haris <muhammad.brian.27@gmail.com>
2024-01-31 21:40:31 +0700

    PATH PARAMETERS WITH PARAM DECORATORS

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Put, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param } from '@nestjs/common';
+import { ReportType, data } from './data';
 
 @Controller('report/:type')
 export class AppController {
   @Get('')
-  getAllReports() {
-    return ['getAllReports'];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,7 @@
-export const data: Data = {
-  report: [],
-};
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
 
 interface Data {
   report: {
@@ -13,16 +14,40 @@ interface Data {
   }[];
 }
 
-enum ReportType {
-  INCOME = 'income',
-  EXPENSE = 'expense',
-}
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'YouTube',
+      amount: 2500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
 
-data.report.push({
-  id: 'uuid',
-  source: 'Salary',
-  amount: 7500,
-  created_at: new Date(),
-  updated_at: new Date(),
-  type: ReportType.INCOME,
-});
+// data.report.push({
+//   id: 'uuid',
+//   source: 'Salary',
+//   amount: 7500,
+//   created_at: new Date(),
+//   updated_at: new Date(),
+//   type: ReportType.INCOME,
+// });
